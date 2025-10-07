4a8d8cb9e232e5330984cb1fc014c188f0811f39
Fredrik Schjølberg Husebø <fredriksh@uia.no>
2025-10-07 10:20:10 +0200

    include i index

--- public/Index.php
+++ public/Index.php
@@ -1,25 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<meta charset="UTF-8">
-<title>Index</title>
-<meta name="viewport" content="width=device-width,initial-scale=1">
-<link rel="stylesheet" href="./CSS/Index.css">
-<body>
-   <?php include '../app/controllers/dbController.php';?>
+
+<?php include '../app/views/_header.php';?>
+<?php include '../app/controllers/dbController.php';?>
 
 <div class="chatty">
- <h1 id="overskrift">Chatbot</h1>
- <form method="POST">
-      <input type="submit" name="createdb"
-                class="button" value="Create database"/>
+   <h1 id="overskrift">Chatbot</h1>
+   <form method="POST">
+      <input type="submit" name="createdb" class="button" value="Create database"/>
    </form>
-   <br>
- <form action="">
-    Input : <input type="text" placeholder="Elias har liten tiss" name="" id=""> <br>
-    Output : <output placeholder="Output"></output>
-    
- </form>
+   <form action="">
+      Input : <input type="text" placeholder="Elias har liten tiss" name="" id=""> <br>
+      Output : <output placeholder="Output"></output>
+   </form>
 </div>
 
-</body>
-</html>
+
+<?php include '../app/views/_footer.php';?>
