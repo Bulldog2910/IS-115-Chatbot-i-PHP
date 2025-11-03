54ce0b23b1c62dd7b313108a51fed6fdb00d6f65
Fredrik Schjølberg Husebø <fredriksh@uia.no>
2025-11-03 13:40:19 +0100

    Keyword baserte svar funker som proof of concept

--- public/Index.php
+++ public/Index.php
@@ -1,17 +1,33 @@
+<?php 
+session_start();
 
-<?php include '../app/views/_header.php';?>
-<?php include '../app/controllers/dbController.php';?>
+// Get values from session if they exist
+$message = $_SESSION['message'] ?? '';
+$output = $_SESSION['output'] ?? '';
+$error = $_SESSION['error'] ?? [];
+
+// Clear session values after displaying
+unset($_SESSION['message'], $_SESSION['output'], $_SESSION['error']);
+
+include '../app/views/_header.php';
+include '../app/controllers/dbController.php';
+include '../app/controllers/chatbotController.php';
+?>
 
 <div class="chatty">
    <h1 id="overskrift">Chatbot</h1>
-   <form method="POST">
+   <form method="POST" action="../app/controllers/ProcessController.php">
       <input type="submit" name="createdb" class="button" value="Create database"/>
    </form>
-   <form action="">
-      Input : <input type="text" placeholder="Input" name="" id=""> <br>
-      Output : <output placeholder="Output"></output>
+
+   <form method="POST" action="../app/controllers/ProcessController.php">
+      Fornavn: <input type="text" name="message" placeholder="Skriv her..." value="<?php echo htmlspecialchars($message); ?>"><br>
+      <?php if (isset($error['Message'])): ?>
+         <span style="color: red;"><?php echo $error['Message']; ?></span><br>
+      <?php endif; ?>
+      Output : <output><?php echo htmlspecialchars($output); ?></output><br>
+      <input type="submit" name="Send" value="Send">
    </form>
 </div>
-
 
 <?php include '../app/views/_footer.php';?>
