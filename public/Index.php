805e2f2716941b0beeb4ee24e9cee80408647109
Bulldog2910 <mat.nik.jorgensen@gmail.com>
2025-11-03 13:11:46 +0100

    Automatisk creation av database
    - Hvis database eksisterer så skipper den
    - Hvis den ikke finnes så lager den en database og tablesene inni

--- public/Index.php
+++ public/Index.php
@@ -1,12 +1,9 @@
 
-<?php include '../app/views/_header.php';?>
-<?php include '../app/controllers/dbController.php';?>
+<?php include __DIR__ . '/../app/views/_header.php';?>
+<?php include __DIR__ . '/../app/controllers/dbController.php';?>
 
 <div class="chatty">
    <h1 id="overskrift">Chatbot</h1>
-   <form method="POST">
-      <input type="submit" name="createdb" class="button" value="Create database"/>
-   </form>
    <form action="">
       Input : <input type="text" placeholder="Input" name="" id=""> <br>
       Output : <output placeholder="Output"></output>
