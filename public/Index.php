5fea10547ec8b37d6139318895868db126428b29
Fredrik Schjølberg Husebø <fredriksh@uia.no>
2025-11-25 18:47:23 +0100

    index fiks

--- public/Index.php
+++ public/Index.php
@@ -3,8 +3,5 @@
 
 <?php include __DIR__ . '/../app/views/chatbotView.php';?>
 
-<a class="button" href="../app/views/userCreation.php">User creation</a>
-<a class="button" href="../app/controllers/chatbotControllerTest.php">chatbotTest</a>
-<a class="button" href="../app/views/admin.php">admin</a>
 
 <?php include '../app/views/_footer.php';?>
