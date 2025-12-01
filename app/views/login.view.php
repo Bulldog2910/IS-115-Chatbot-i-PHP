d4566cb9148dc8a6391a299cb0b7a9fee0ad90dc
Fredrik Schjølberg Husebø <fredriksh@uia.no>
2025-12-01 11:34:14 +0100

    Translations

--- app/views/login.view.php
+++ app/views/login.view.php
@@ -18,12 +18,12 @@
 
     <form method="POST" action="">
         <div class="form-group">
-            <label for="email">E-post:</label>
+            <label for="email">Email:</label>
             <input type="email" id="email" name="email" required value="<?php echo htmlspecialchars($email, ENT_QUOTES, 'UTF-8'); ?>" placeholder="Your e-mail">
         </div>
 
         <div class="form-group">
-            <label for="password">Passord:</label>
+            <label for="password">Password:</label>
             <input type="password" id="password" name="password" required placeholder="Passord">
         </div>
 
--- app/views/registerUser.view.php
+++ app/views/registerUser.view.php
@@ -6,44 +6,47 @@
 ?>
 <link rel="stylesheet" href="./CSS/registrerUser.css">
 
-<h1>Registrer ny bruker</h1>
+
 
 <?php if (!empty($error)): ?>
-    <ul style="color:red;">
-        <?php foreach ($error as $msg): ?>
-            <li><?= htmlspecialchars($msg, ENT_QUOTES, 'UTF-8') ?></li>
-        <?php endforeach; ?>
-    </ul>
+       <ul style="color:red;">
+              <?php foreach ($error as $msg): ?>
+              <li><?= htmlspecialchars($msg, ENT_QUOTES, 'UTF-8') ?></li>
+              <?php endforeach; ?>
+       </ul>
 <?php endif; ?>
 
 <?php if (!empty($successMsg)): ?>
-    <p style="color:green;"><?= htmlspecialchars($successMsg, ENT_QUOTES, 'UTF-8') ?></p>
+       
 <?php endif; ?>
 
 <form method="post" action="">
-    <label for="firstName">Fornavn:</label><br>
-    <input type="text" id="firstName" name="firstName"
-           value="<?= htmlspecialchars($data['firstName'] ?? '', ENT_QUOTES, 'UTF-8') ?>"><br><br>
 
-    <label for="lastName">Etternavn:</label><br>
-    <input type="text" id="lastName" name="lastName"
-           value="<?= htmlspecialchars($data['lastName'] ?? '', ENT_QUOTES, 'UTF-8') ?>"><br><br>
+       <h1>Register new user</h1>
 
-    <label for="username">Brukernavn:</label><br>
-    <input type="text" id="username" name="username"
-           value="<?= htmlspecialchars($data['username'] ?? '', ENT_QUOTES, 'UTF-8') ?>"><br><br>
+       <label for="firstName">First name:</label><br>
+       <input type="text" id="firstName" name="firstName"
+              value="<?= htmlspecialchars($data['firstName'] ?? '', ENT_QUOTES, 'UTF-8') ?>"><br><br>
 
-    <label for="userpassword">Passord:</label><br>
-    <input type="password" id="userpassword" name="userpassword"
-           value="<?= htmlspecialchars($data['userpassword'] ?? '', ENT_QUOTES, 'UTF-8') ?>"><br><br>
+       <label for="lastName">Last name:</label><br>
+       <input type="text" id="lastName" name="lastName"
+              value="<?= htmlspecialchars($data['lastName'] ?? '', ENT_QUOTES, 'UTF-8') ?>"><br><br>
 
-    <label for="repeatpassword">Gjenta passord:</label><br>
-    <input type="password" id="repeatpassword" name="repeatpassword"
-           value="<?= htmlspecialchars($data['repeatpassword'] ?? '', ENT_QUOTES, 'UTF-8') ?>"><br><br>
+       <label for="username">Username:</label><br>
+       <input type="text" id="username" name="username"
+              value="<?= htmlspecialchars($data['username'] ?? '', ENT_QUOTES, 'UTF-8') ?>"><br><br>
 
-    <label for="mail">E-post:</label><br>
-    <input type="text" id="mail" name="mail"
-           value="<?= htmlspecialchars($data['mail'] ?? '', ENT_QUOTES, 'UTF-8') ?>"><br><br>
+       <label for="userpassword">Password:</label><br>
+       <input type="password" id="userpassword" name="userpassword"
+              value="<?= htmlspecialchars($data['userpassword'] ?? '', ENT_QUOTES, 'UTF-8') ?>"><br><br>
 
-    <input type="submit" name="registrer" value="Register">
+       <label for="repeatpassword">Repeat password:</label><br>
+       <input type="password" id="repeatpassword" name="repeatpassword"
+              value="<?= htmlspecialchars($data['repeatpassword'] ?? '', ENT_QUOTES, 'UTF-8') ?>"><br><br>
+
+       <label for="mail">Email:</label><br>
+       <input type="text" id="mail" name="mail"
+              value="<?= htmlspecialchars($data['mail'] ?? '', ENT_QUOTES, 'UTF-8') ?>"><br><br>
+
+       <input type="submit" name="registrer" value="Register">
 </form>
